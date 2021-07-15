ce75b756c0984a88abe83131aeeb276bafa283aa
sneharghya-pathak <sneharghya.pathak.contractor@oup.com>
2021-07-15 20:31:22 +0530

    added code

--- src/main/java/com/pathaks/camel/beans/TestBean.java
+++ src/main/java/com/pathaks/camel/beans/TestBean.java
@@ -1,6 +1,15 @@
 package com.pathaks.camel.beans;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestBean {
-	//to do
+
+	public String twoChars(String input) {
+		return input.substring(0, 2);
+	}
+
+	public String append(String existing, String next) {
+		return existing + next;
+	}
 }
--- src/main/java/com/pathaks/camel/route/ExampleRoute.java
+++ src/main/java/com/pathaks/camel/route/ExampleRoute.java
@@ -1,12 +1,26 @@
 package com.pathaks.camel.route;
 
+import com.pathaks.camel.beans.TestBean;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.util.toolbox.AggregationStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class ExampleRoute extends RouteBuilder {
+
+	@Autowired
+	TestBean testBean;
+
 	@Override
 	public void configure() throws Exception {
-		from("timer:foo?repeatCount=1")
-		.log("test message");
+		from("file:inbox")
+		.split(body().tokenize("\n"))
+			.setHeader("correlationKey").method(testBean, "twoChars(${body})")
+			.log("${header.correlationKey}")
+			.aggregate(header("correlationKey"), AggregationStrategies.bean(testBean, "append"))
+				.completionTimeout(5000)
+				.to("log:loggerName?showBody=false&showHeaders=true&showProperties=true&multiline=true")
+				.to("file:outbox?fileName=${header.correlationKey}.txt");
 	}
 }
